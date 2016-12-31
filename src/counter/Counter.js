71d9563c4dc6619e3a4a24e91d05e8a01480bac7
uryyyyyyy <koki305@gmail.com>
2016-12-31 15:58:47 +0900

    redux

---
+++ src/counter/Counter.js
@@ -0,0 +1,57 @@
+// @flow
+
+import React, {Component} from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button
+} from 'react-native';
+import {CounterState} from "./Entities";
+import {ActionDispatcher} from "./Actions";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+type Props = {
+  value: CounterState;
+  actions: ActionDispatcher;
+};
+
+export class Counter extends Component<void, Props, void> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          {`Count: ${this.props.value.num}`}
+        </Text>
+        <Button
+          onPress={() => this.props.actions.increment(3)}
+          title="Increment 3"
+          color="#841584"
+        />
+        <Button
+          onPress={() => this.props.actions.decrement(2)}
+          title="decrement 2"
+          color="#841584"
+        />
+      </View>
+    );
+  }
+}
